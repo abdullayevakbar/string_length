8f91d5af36286a6c45be4136ad7af0e17d0e7592
Akbar Abdullayev <abdullayevakbar0101@gmail.com>
2022-07-02 16:22:02 +0500

    string

--- bin/string_length07.dart
+++ bin/string_length07.dart
@@ -8,10 +8,11 @@
         string
 */
 String func(String s1, String s2) {
-  if ((s1.length) % 2 == 1 && (s2.length) % 2 == 1) return s1;
+  if ((s1.length + s2.length) % 2 == 0) return s1;
   return s2;
 }
 
 void main() {
+  print(func("as1", "aass"));
   // write your code here
 }
